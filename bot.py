b011ee00ea2e4d83518c594935aca496bc45ac86
Gorg1969 <gorgnik@gmail.com>
2026-07-20 13:40:27 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,4 +68,4 @@ async def websocket_endpoint(websocket: WebSocket):
 if __name__ == "__main__":
     init_db()
     print("🚀 Запуск бота (без Webhook)...")
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
